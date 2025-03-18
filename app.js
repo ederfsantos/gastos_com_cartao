d348f3f4917554f8cba19418e0e68784fc0113e0
Eder Ferreira Santos <ederfsantos@live.com>
2025-03-18 12:46:22 -0300

    adicionando campo de definicao de meta pelo usuario.

--- app.js
+++ app.js
@@ -4,26 +4,27 @@
 let nubank;
 let c6;
 let credicard;
-const meta = 2100;
-
+//const meta = 2100;
+let meta;
 
 function sortearAmigo() {
+  meta = parseFloat(document.getElementById('meta').value.replace(",", "."));
   nubank = parseFloat(document.getElementById('nubank').value.replace(",", "."));
   c6 = parseFloat(document.getElementById('c6').value.replace(",", "."));
   credicard = parseFloat(document.getElementById('credicard').value.replace(",", "."));
   console.log(nubank);
   console.log(c6);
   console.log(credicard);
-  
-    let totalDeGastos = (nubank + c6 + credicard).toFixed(2);
-    console.log(totalDeGastos);
-    let valorDisponivel = (meta - totalDeGastos).toFixed(2);
-    console.log(valorDisponivel);
-    alert("A sua meta de gastos é de R$ " + meta.toFixed(2) + "\nO total de gastos com cartões foi de R$ "
-      + totalDeGastos + "\nO valor disponível para gastos é de RS " + valorDisponivel);
-    let lista = document.getElementById('resultado');
-    lista.innerHTML = "<h2>A sua meta de gastos é de R$ " + meta.toFixed(2) + "\nO total de gastos com cartões foi de R$ " + totalDeGastos + "\nO valor disponivel para gastos é de RS " + valorDisponivel + "</h2>";
 
-  
+  let totalDeGastos = (nubank + c6 + credicard).toFixed(2);
+  console.log(totalDeGastos);
+  let valorDisponivel = (meta - totalDeGastos).toFixed(2);
+  console.log(valorDisponivel);
+  alert("A sua meta de gastos é de R$ " + meta.toFixed(2) + "\nO total de gastos com cartões foi de R$ "
+    + totalDeGastos + "\nO valor disponível para gastos é de RS " + valorDisponivel);
+  let lista = document.getElementById('resultado');
+  lista.innerHTML = "<h2>A sua meta de gastos é de R$ " + meta.toFixed(2) + "\nO total de gastos com cartões foi de R$ " + totalDeGastos + "\nO valor disponivel para gastos é de RS " + valorDisponivel + "</h2>";
+
+
 }
 
