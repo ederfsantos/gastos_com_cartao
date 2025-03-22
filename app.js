76c35bcac6d62dbeff26fa3f79ec6a99574151b1
Eder Ferreira Santos <ederfsantos@live.com>
2025-03-21 23:39:04 -0300

    removendo nomes fixos de cartoes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function sortearAmigo() {
   alert("A sua meta de gastos é de R$ " + meta.toFixed(2) + "\nO total de gastos com cartões foi de R$ "
     + totalDeGastos + "\nO valor disponível para gastos é de RS " + valorDisponivel);
   let lista = document.getElementById('resultado');
-  lista.innerHTML = "<h2>A sua meta de gastos é de R$ " + meta.toFixed(2) + "\nO total de gastos com cartões foi de R$ " + totalDeGastos + "\nO valor disponivel para gastos é de RS " + valorDisponivel + "</h2>";
+  lista.innerHTML = "<h2>A sua meta de gastos é de R$ " + meta.toFixed(2) + "\nO total de gastos com cartões foi de R$ " + totalDeGastos + "\nO valor disponível para gastos é de RS " + valorDisponivel + "</h2>";
 
 
 }
